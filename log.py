2e1794bdffd9e4fd700f912686539a8f15b5f8b2
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-11-11 22:50:46 +0700

    Add option to custom message

--- log.py
+++ log.py
@@ -1,4 +1,5 @@
 from datetime import time
+from string import Template
 import time
 import yaml
 from git import Repo
@@ -7,20 +8,26 @@ def first_line_of(commit_message):
     lines = commit_message.split("\n")
     return lines[0]
 
+def transform_commit(commit_data):
+    return {
+        'date': time.strftime('%d-%b-%y', time.localtime(commit_data['commit'].committed_date)),
+        'project_name': commit_data['project'],
+        'hash': str(commit_data['commit'].hexsha),
+        'short_hash': str(commit_data['commit'].hexsha)[0:7],
+        'message_1_line': first_line_of(commit_data['commit'].summary),
+        'message': commit_data['commit'].summary
+    }
 
-def print_commit(project_name, commit):
-    print("{time} [{project_name}] {msg} #{hash}".format(
-        time = time.strftime('%Y-%m-%d', time.localtime(commit.committed_date)),
-        project_name = project_name,
-        hash = str(commit.hexsha)[0:7],
-        msg = first_line_of(commit.summary)
-        ))
+def print_commit(template, commit_data):
+    tmpl = Template(template)
+    print(tmpl.substitute(**transform_commit(commit_data)))
 
 if __name__ == "__main__":
     all_commits = {}
     with open("./log.yml", "r") as yaml_file:
         data = yaml.load(yaml_file, Loader=yaml.CLoader)
         projects = data['projects']
+        message_template = data['message']
 
         for project in projects:
             project_name = project['name']
@@ -31,8 +38,8 @@ if __name__ == "__main__":
                     hash = str(commit.hexsha)
                     all_commits[hash] = { 'project': project_name, 'commit': commit}
 
-    commits = all_commits.values()
-    commits = sorted(commits, key=lambda data: data['commit'].committed_date, reverse=True)
+        commits = all_commits.values()
+        commits = sorted(commits, key=lambda data: data['commit'].committed_date, reverse=data['reverse'])
 
-    for commit in commits:
-        print_commit(commit['project'], commit['commit'])
+        for commit in commits:
+            print_commit(message_template, commit)
